d5a6f1004925c1b9fd1efb5781c08dd755eb694e
NITHANR <nithanr39@gmail.com>
2024-07-26 21:01:04 +0530

    feat:Completed SelectionSort

--- SORTING/SelectionSort.c
+++ SORTING/SelectionSort.c
@@ -1,6 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+void print(const int *,int );
+void SelectionSort(int*,int);
+void swap(int *,int *);
+
 int main(){
     int arr[] = {10,20,30,11,25,70,90,68,55};
+    int arr_size;
+    arr_size=SIZE(arr);
+    puts("Before Selection Sort: ");
+    print(arr,arr_size);
+    SelectionSort(arr,arr_size);
+    puts("\nAfter Selection Sort :");
+    print(arr,arr_size);
     return  EXIT_SUCCESS;
+}
+
+void print(const int * arr,int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void SelectionSort(int *arr,int size){
+    int mini;
+    int mini_pos;
+    for(int i=0;i<size;i++){
+        mini_pos=i;
+        for(int j=i+1;j<size;j++){
+            if(arr[j]<arr[mini_pos]){
+                mini_pos=j;
+            }
+        }
+        swap((arr+mini_pos),(arr+i));
+    }
+
 }
