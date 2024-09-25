f53aa4eb19ed2cc84b60fb4894c9fdb100ef22a1
NITHANR <nithanr39@gmail.com>
2024-09-25 07:41:43 +0530

    insert functionality added to AVL

--- TREE/AVL.cpp
+++ TREE/AVL.cpp
@@ -38,4 +38,82 @@ public:
             }
         }
     }
+
+    int getBalanceFactor(Treenode* r){
+        if(r==NULL){
+            return -1;
+        }
+        else{
+            return (height(r->left) - height(r->right));
+        }
+    }
+
+    Treenode* rightRotate(Treenode* r){
+        Treenode* x =r->left;
+        Treenode* SubTree =x->right;
+        // roations
+        x->right=r;
+        r->left=SubTree;
+        return x;
+    }
+
+    Treenode* leftRotate(Treenode* r){
+        Treenode* x = r->right;
+        Treenode* subTree = x->left;
+        // rotation
+        x->left=r;
+        r->right=subTree;
+        return x;
+    }
+
+    Treenode* insert(Treenode* r,Treenode* new_node){
+        if(r==NULL){
+            r=new_node;
+            cout<<"Inserted at root\n";
+            return r;
+        }
+
+        if(new_node->value<r->value){
+            r->left=insert(r->left,new_node);
+        }
+        else if(new_node->value>r->value){
+            r->right=insert(r->right,new_node);
+        }
+        else{
+            cout<<"No duplication allowed\n";
+            return r;
+        }
+
+        int bf = getBalanceFactor(r);
+
+        // ll imbal
+        if(bf>1 && new_node->value<r->left->value){
+            return rightRotate(r);
+        }
+
+        // rr imbal
+        if(bf<-1 && new_node->value>r->right->value){
+            return leftRotate(r);
+        }
+
+        // lr imbal
+        if(bf>1 && new_node->value>r->left->value){
+            r->left=leftRotate(r->left);
+            return rightRotate(r);
+        }
+
+        // rl imbal
+        if(bf<-1 && new_node->value<r->right->value){
+            r->right=rightRotate(r->right);
+            return leftRotate(r);
+        }
+
+        return r;
+    }
+
 };
+
+int main(){
+    AVLTree obj;
+
+}
